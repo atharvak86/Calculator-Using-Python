e34f0b91d85bf9d410509d7de4b38456ab8127b3
ATHARVA DINESH KHADSE <84701286+atharvak86@users.noreply.github.com>
2022-04-17 15:07:55 +0530

    Create Calculator.py
    Calculator using Python. It can be used to calculate addition, substraction, multiplication, division and modulo. User have to provide input for a and b. After that it will show the result

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+print("||Calculator||")
+print("Enter value of two numbers a and b")
+a=int(input())
+b=int(input())
+print("The value of a is", a)
+print("The value of b is", b)
+sum=a+b
+sub=a-b
+mul=a*b
+div=int(a/b)
+mod=a%b
+print("Addition of two numbers is", sum)
+print("Substraction of two numbers is", sub)
+print("Multiplication of two numbers is", mul)
+print("Division of two numbers is", div)
+print("Modulo or Remainder of two numbers is", mod)
